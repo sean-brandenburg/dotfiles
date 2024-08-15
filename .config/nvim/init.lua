31ad745c43045a9a279c16f990e3e2a3331e1638
Sean Brandenburg <seanbrandenburg333@gmail.com>
2024-08-15 14:15:16 -0400

    fix init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,8 +4,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require 'sean.plugins.remap'
-
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
