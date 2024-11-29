19d13c8d13a35cda47f9e84ee2cf0f8875e0b2df
sean333 <seanbrandenburg333@gmail.com>
2024-11-29 00:37:14 -0500

    cleanup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,31 +21,7 @@ require('lazy').setup {
   },
 
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
   -- 'airblade/vim-rooter', -- set cwd to project root (this was not working properly for me)
-
-  {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-
-  -- Highlight todo, notes, etc in comments
-  {
-    'folke/todo-comments.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-      signs = false,
-    },
-  },
 
   { import = 'sean.plugins' },
   { import = 'sean.plugins.lsp' },
