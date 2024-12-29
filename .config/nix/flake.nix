60d399a6d5e6788519e99611ebcfc8bd21e1e34d
sean333 <seanbrandenburg333@gmail.com>
2024-12-29 15:43:12 -0700

    nix init

---
+++ .config/nix/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Sean's Nix-Darwin System Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.neovim
+        ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      nixpkgs.hostPlatform = "x86_64-darwin"; # "aarch64-darwin" for M series chips
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#personal
+    darwinConfigurations."personal" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
