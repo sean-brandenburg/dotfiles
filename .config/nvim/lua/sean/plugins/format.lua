16839c01ab05a371089160951985e9e6940c4654
sean-brandenburg <seanbrandenburg333@gmail.com>
2025-08-02 18:46:45 -0400

    turn off err on fail to lint

--- .config/nvim/lua/sean/plugins/format.lua
+++ .config/nvim/lua/sean/plugins/format.lua
@@ -12,7 +12,7 @@ return { -- Autoformat
     },
   },
   opts = {
-    notify_on_error = true,
+    notify_on_error = false,
     format_on_save = function(bufnr)
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
