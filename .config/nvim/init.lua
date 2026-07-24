05cf23287d3cd4751697cf28d674571a19f41488
sean-brandenburg <seanbrandenburg333@gmail.com>
2026-07-23 21:31:59 -0400

    clear vim background

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,13 @@ require('lazy').setup {
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
+      require('tokyonight').setup {
+        transparent = true,
+        styles = {
+          sidebars = 'transparent',
+          floats = 'transparent',
+        },
+      }
       vim.cmd.colorscheme 'tokyonight-night'
       vim.cmd.hi 'Comment gui=none'
     end,
